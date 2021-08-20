9afe73445de173a36b1b60db931ee5b0053003c6
farel015 <farelakbar90@gmail.com>
2021-08-21 00:23:46 +0700

    init awal

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  purge: ["./public/**/*.html"], //file index.html berada di folder public
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
